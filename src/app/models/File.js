1aef8bc5578927d831ab39343d54ae3837403648
Manogel <manoel_ggomes@hotmail.com>
2020-01-27 12:17:14 -0300

    serving static files and adjustments

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://${process.env.HOST}:${process.env.PORT}/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'mysql', // or 'postgres' and yarn add pg-hstore or https://sequelize.org/master/manual/dialects.html
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'minha_cidade',
+  dialect: process.env.DB_CONNECTION, // or 'postgres' and yarn add pg-hstore or https://sequelize.org/master/manual/dialects.html
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
