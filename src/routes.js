471f2fa4222d28c41a6e5005dadb17b0fa12a39c
Manogel <manoelgomes53@gmail.com>
2020-04-02 23:18:20 -0300

    jest config

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,17 @@ const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/users', UserController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 // Rotas privadas apartir daqui
 routes.put('/users', UserController.update);
+
+routes.get('/dashboard', (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
